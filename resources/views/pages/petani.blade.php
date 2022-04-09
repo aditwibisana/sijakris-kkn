024cb3e0ed118cc82c287e38b16caae4ef7471be
Bayu Adhitya Wibisana <b.awibisana@gmail.com>
2022-04-10 04:04:14 +0800

    Tambah Petani dan Login

--- resources/views/pages/petani.blade.php
+++ resources/views/pages/petani.blade.php
@@ -50,8 +50,7 @@
     <div class="full-title">
         <div class="container">
             <!-- Page Heading/Breadcrumbs -->
-            <h1 class="mt-4 mb-3">Portfolio 1
-                <small>Subheading</small>
+            <h1 class="mt-4 mb-3">Daftar Petani
             </h1>
         </div>
     </div>
@@ -61,119 +60,41 @@
         <div class="breadcrumb-main">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item">
-                    <a href="index.html">Home</a>
+                    <a href="/">Beranda</a>
                 </li>
-                <li class="breadcrumb-item active">Portfolio 1</li>
+                <li class="breadcrumb-item active">Daftar Petani</li>
             </ol>
         </div>
+        @foreach ($data as $item)
         <div class="project-inner">
             <!-- Project One -->
             <div class="row">
                 <div class="col-md-7">
                     <a href="#">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="images/portfolio-big-01.jpg" alt="" />
+                        <img class="img-fluid rounded mb-3 mb-md-0" src="{{$item->photo}}" alt="" height="400px" width="600px" />
                     </a>
                 </div>
                 <div class="col-md-5">
-                    <h3>Morbi eu nulla ac mauris euismod aliquet.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-                    <p>Suspendisse potenti. Suspendisse potenti. Phasellus rutrum metus at odio consectetur tempus. Pellentesque sed tellus et ligula luctus auctor et non erat.</p>
-                    <a class="btn btn-primary" href="#">View Project
+                    <h2>{{$item->name}}</h2>
+                    <p>No Telepon: {{$item->contact}}</p>
+                    <p>Daftar Tanaman:</p>
+                    <p>{{$item->list_of_plant}}</p>
+                    <a class="btn btn-primary" href="#">Hubungi
                         <span class="glyphicon glyphicon-chevron-right"></span>
                     </a>
                 </div>
             </div>
             <!-- /.row -->
-        </div>
-        <hr>
-        <div class="project-inner">
-            <!-- Project Two -->
-            <div class="row">
-                <div class="col-md-7">
-                    <a href="#">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="images/portfolio-big-02.jpg" alt="" />
-                    </a>
-                </div>
-                <div class="col-md-5">
-                    <h3>Morbi eu nulla ac mauris euismod aliquet.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-                    <p>Suspendisse potenti. Suspendisse potenti. Phasellus rutrum metus at odio consectetur tempus. Pellentesque sed tellus et ligula luctus auctor et non erat.</p>
-                    <a class="btn btn-primary" href="#">View Project
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-            <!-- /.row -->
-        </div>
-        <hr>
-        <div class="project-inner">
-            <!-- Project Three -->
-            <div class="row">
-                <div class="col-md-7">
-                    <a href="#">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="images/portfolio-big-03.jpg" alt="" />
-                    </a>
-                </div>
-                <div class="col-md-5">
-                    <h3>Morbi eu nulla ac mauris euismod aliquet.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-                    <p>Suspendisse potenti. Suspendisse potenti. Phasellus rutrum metus at odio consectetur tempus. Pellentesque sed tellus et ligula luctus auctor et non erat.</p>
-                    <a class="btn btn-primary" href="#">View Project
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-            <!-- /.row -->
-        </div>
-        <hr>
-
-        <!-- Project Four -->
-        <div class="project-inner">
-            <div class="row">
-                <div class="col-md-7">
-                    <a href="#">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="images/portfolio-big-04.jpg" alt="" />
-                    </a>
-                </div>
-                <div class="col-md-5">
-                    <h3>Morbi eu nulla ac mauris euismod aliquet.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-                    <p>Suspendisse potenti. Suspendisse potenti. Phasellus rutrum metus at odio consectetur tempus. Pellentesque sed tellus et ligula luctus auctor et non erat.</p>
-                    <a class="btn btn-primary" href="#">View Project
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-            <!-- /.row -->
+            <hr>
         </div>
 
-        <hr>
-
-        <div class="pagination_bar">
-            <!-- Pagination -->
-            <ul class="pagination justify-content-center">
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#">1</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#">2</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#">3</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </li>
-            </ul>
+        @endforeach
+        <div class="pagination">
+            <nav aria-label="Page navigation example">
+                <ul>
+                    {{$data->links()}}
+                </ul>
+            </nav>
         </div>
     </div>
     <!-- /.container -->
